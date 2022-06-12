fbb3d3ade4157ded205411db5ec891a353a9b80c
Gonzalo Vittori <gonzov@gmail.com>
2022-06-12 16:20:22 -0300

    Update GAconfig.js

--- js/GAconfig.js
+++ js/GAconfig.js
@@ -6,10 +6,10 @@ const GAconfig = {
     "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
     "client_secret": "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",
     "property_id": 317628904, //nombre: test
-    "redirect_uris": [
-        "https://gvittori.github.io/home/"
-    ],
-    "javascript_origins": [
-        "https://gvittori.github.io/home/
-    ]
+    // "redirect_uris": [
+    //     "https://gvittori.github.io/home/"
+    // ],
+    // "javascript_origins": [
+    //     "https://gvittori.github.io/home/
+    // ]
 };
